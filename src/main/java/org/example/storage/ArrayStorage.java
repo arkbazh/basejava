a2308f9d3a82f856322dcc006478d8d97752b47e
arkbazh <arkbazh@gmail.com>
2026-01-11 21:45:30 -0800

    Implement abstract and concrete storage classes for Resumes, add testing infrastructure, and define utility models.

---
+++ src/main/java/org/example/storage/ArrayStorage.java
@@ -0,0 +1,31 @@
+package storage;
+
+import model.Resume;
+
+/**
+ * Array-based storage for Resumes.
+ */
+public class ArrayStorage extends AbstractArrayStorage {
+
+    @Override
+    protected int findIndex(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (uuid.equals(storage[i].getUuid())) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    protected void insertResume(Resume r, int index) {
+        storage[size] = r;
+    }
+
+    @Override
+    protected void deleteResume(int index) {
+        size--;
+        storage[index] = storage[size];
+    }
+
+}
