b70f74231f8e279cff1b02a4df9fad4cde78d61d
arkbazh <arkbazh@gmail.com>
2026-01-19 20:03:53 -0800

    refactor(main): remove unused methods

--- src/main/java/org/example/MainArray.java
+++ src/main/java/org/example/MainArray.java
@@ -1,25 +1,25 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import org.example.model.Resume;
 import org.example.storage.ArrayStorage;
 import org.example.storage.Storage;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 /**
- * Test for ru.javawebinar.basejava.storage.ArrayStorage
+ * Test for ru.javawebinar.basejava.storage.ArrayStorage.
  */
 public class MainArray {
-    private final static Storage ARRAY_STORAGE = new ArrayStorage();
+    private static final Storage ARRAY_STORAGE = new ArrayStorage();
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         Resume r;
         while (true) {
             System.out.print(
-                    "Введите одну из команд - (list | save uuid | delete uuid | get uuid | update uuid | clear | exit): ");
+                    "Введите одну из команд - (list | save uuid | delete uuid " +
+                            "| get uuid | update uuid | clear | exit): ");
             String[] params = reader.readLine().trim().toLowerCase().split(" ");
             if (params.length < 1 || params.length > 2) {
                 System.out.println("Неверная команда.");
@@ -38,13 +38,11 @@ public class MainArray {
                     break;
                 case "save":
                     r = new Resume();
-                    r.setUuid(uuid);
                     ARRAY_STORAGE.save(r);
                     printAll();
                     break;
                 case "update":
                     r = new Resume();
-                    r.setUuid(uuid);
                     ARRAY_STORAGE.update(r);
                     printAll();
                     break;
