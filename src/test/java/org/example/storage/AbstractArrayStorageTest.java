7348983269a3ac9769f3e56877844c7fb76db448
arkbazh <arkbazh@gmail.com>
2026-01-31 21:36:08 -0800

    refactor(test): small changes and rename methods  AstractArrayStorageTest

--- src/test/java/org/example/storage/AbstractArrayStorageTest.java
+++ src/test/java/org/example/storage/AbstractArrayStorageTest.java
@@ -14,12 +14,16 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 abstract class AbstractArrayStorageTest {
-    private static final Resume R1 = new Resume("uuid1");
-    private static final Resume R2 = new Resume("uuid2");
-    private static final Resume R3 = new Resume("uuid3");
-    private static final Resume R4 = new Resume("uuid4");
+    private static final String UUID_1 = "uuid1";
+    private static final String UUID_2 = "uuid2";
+    private static final String UUID_3 = "uuid3";
+    private static final String UUID_4 = "uuid4";
     private static final String MISSING = "missing";
     private static final String OVERFLOW = "overflow";
+    private static final Resume R1 = new Resume(UUID_1);
+    private static final Resume R2 = new Resume(UUID_2);
+    private static final Resume R3 = new Resume(UUID_3);
+    private static final Resume R4 = new Resume(UUID_4);
 
     private final Storage storage;
 
@@ -36,25 +40,25 @@ abstract class AbstractArrayStorageTest {
     }
 
     @Test
-    void save_whenEmpty_saves() {
+    void save_whenStorageEmpty_savesResume() {
         storage.clear();
         storage.save(R1);
-        assertEquals(R1, storage.get(R1.getUuid()));
+        assertEquals(R1, storage.get(UUID_1));
     }
 
     @Test
-    void save_whenNotEmpty_saves() {
+    void save_whenStorageNotEmpty_savesResume() {
         storage.save(R4);
-        assertEquals(R4, storage.get(R4.getUuid()));
+        assertEquals(R4, storage.get(UUID_4));
     }
 
     @Test
-    void save_whenExists_throwsResumeAlreadyExistsException() {
+    void save_whenResumeExists_throwsResumeAlreadyExistsException() {
         assertThrows(ResumeAlreadyExistsException.class, () -> storage.save(R1));
     }
 
     @Test
-    void save_whenFull_throwsStorageOverflowException() {
+    void save_whenStorageFull_throwsStorageOverflowException() {
         storage.clear();
         int limit = AbstractArrayStorage.STORAGE_LIMIT;
 
@@ -71,59 +75,56 @@ abstract class AbstractArrayStorageTest {
 
     @Test
     void get_whenResumeExists_returnResume() {
-        Resume actual = storage.get(R1.getUuid());
+        Resume actual = storage.get(UUID_1);
         assertEquals(R1, actual);
     }
 
     @Test
-    void get_whenNotFound_throwsResumeNotFoundException() {
+    void get_whenResumeNotExists_throwsResumeNotFoundException() {
         assertThrows(ResumeNotFoundException.class, () -> storage.get(MISSING));
     }
 
     @Test
-    void update_whenFound_updated() {
-        Resume updated = new Resume(R1.getUuid());
+    void update_whenResumeExists_updatedResume() {
+        Resume updated = new Resume(UUID_1);
 
         storage.update(updated);
 
-        assertSame(updated, storage.get(R1.getUuid()));
+        assertSame(updated, storage.get(UUID_1));
     }
 
     @Test
-    void update_whenNotFound_throwsResumeNotFoundException() {
+    void update_whenResumeNotExists_throwsResumeNotFoundException() {
         Resume updated = new Resume(MISSING);
         assertThrows(ResumeNotFoundException.class, () -> storage.update(updated));
     }
 
     @Test
-    void delete_whenFound_deleted() {
-        storage.delete(R1.getUuid());
-        assertThrows(ResumeNotFoundException.class, () -> storage.get(R1.getUuid()));
+    void delete_whenResumeExists_deletedResume() {
+        storage.delete(UUID_1);
+        assertThrows(ResumeNotFoundException.class, () -> storage.get(UUID_1));
     }
 
     @Test
-    void delete_whenNotFound_throwsResumeNotFoundException() {
+    void delete_whenResumeNotExists_throwsResumeNotFoundException() {
         assertThrows(ResumeNotFoundException.class, () -> storage.delete(MISSING));
     }
 
     @Test
-    void size_whenNotEmpty_returns() {
+    void size_whenStorageNotEmpty_returnsSize() {
         assertEquals(3, storage.size());
     }
 
     @Test
-    void getAll_whenNotEmpty_returns() {
+    void getAll_whenStorageNotEmpty_returnsAllResumes() {
         assertArrayEquals(new Resume[]{R1, R2, R3}, storage.getAll());
     }
 
     @Test
-    void clear_whenNotEmpty_clears() {
+    void clear_whenStorageNotEmpty_clearsStorage() {
+        assertEquals(3, storage.size());
         storage.clear();
         assertArrayEquals(new Resume[0], storage.getAll());
         assertEquals(0, storage.size());
     }
 }
-
-// - в коде 6 раз вызывается R1.getUuid(). Это uuid можно сделать полем
-//
-// - перенеси size--; из deleteResume() в delete, поскольку эта строка дублируется в наследниках AbstractArrayStorage
